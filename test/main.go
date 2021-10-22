9070960fac1774567b792228239a3bb996d64bb7
MJR <m.j.rahman@outlook.com>
2021-10-22 14:32:28 +0100

    files

---
+++ test/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"gr"
+	"strings"
+)
+
+func main() {
+	sliceStr := strings.Split(gr.ReadFile(), " ")
+
+	for i, word := range sliceStr {
+		switch word {
+		case "(hex)":
+			sliceStr[i-1] = gr.ToHex(sliceStr[i-1])
+			sliceStr = append(sliceStr[:i], sliceStr[i+1:]...)
+		case "(bin)":
+			sliceStr[i-1] = gr.ToBin(sliceStr[i-1])
+			sliceStr = append(sliceStr[:i], sliceStr[i+1:]...)
+		case "(up)":
+			sliceStr[i-1] = strings.ToUpper(sliceStr[i-1])
+			sliceStr = append(sliceStr[:i], sliceStr[i+1:]...)
+		case "(low)":
+			sliceStr[i-1] = strings.ToLower(sliceStr[i-1])
+			sliceStr = append(sliceStr[:i], sliceStr[i+1:]...)
+		case "(cap)":
+			sliceStr[i-1] = strings.Title(strings.ToLower(sliceStr[i-1]))
+			sliceStr = append(sliceStr[:i], sliceStr[i+1:]...)
+		}
+	}
+	fmt.Println(sliceStr)
+}
